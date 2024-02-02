1cf9a91882e37541d70f12992ae034b284871ead
BeccaK8 <150313386+BeccaK8@users.noreply.github.com>
2024-02-01 20:36:49 -0600

    setup client side routing and basic nav bar

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
+import AuthPage from '../AuthPage/AuthPage'
+import NewOrderPage from '../NewOrderPage/NewOrderPage'
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
+import NavBar from "../../components/NavBar/NavBar";
+
 export default function App() {
-  return (
-    <main className="App">
-      App
-    </main>
-  );
+
+    const [user, setUser] = useState({});
+
+    return (
+        <main className="App">
+            <NavBar />
+            { user ?
+                <>
+                    <Routes>  
+                        <Route path="/orders/new" element={<NewOrderPage />} />
+                        <Route path="/orders" element={<OrderHistoryPage />} />
+                    </Routes>
+                </>
+                :
+                <AuthPage />
+            }
+        </main>
+    );
 }
